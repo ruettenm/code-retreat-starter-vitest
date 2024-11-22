57e58b16fe3a0f061048690987c1b35bcee70d5b
chris <christoph.butschkau@codecentric.de>
2024-11-22 10:14:12 +0100

    delete all :D

--- game_of_live.spec.ts
+++ game_of_live.spec.ts
@@ -1,52 +1,7 @@
 import {describe, expect, it} from "vitest";
-import {Cell, checkStatusForNextGeneration} from './check_generation_change';
 
 describe("Game Of Live", () => {
-  it("should die if underpopulation", () => {
-    const cell: Cell = {
-      status: "alive",
-      livingNeighbours: 1
-    }
-    expect(checkStatusForNextGeneration(cell)).toBe("dead")
-  })
-
-  describe('2 cells', () => {
-
-  })
-
-  it("should survive if surrounded by 2 living cells", () => {
-    const cell: Cell = {
-      status: "alive",
-      livingNeighbours: 2
-    }
-
-    expect(checkStatusForNextGeneration(cell)).toBe("alive")
-  })
-
-  it("should survive if surrounded by 3 living cells", () => {
-    const cell: Cell = {
-      status: "alive",
-      livingNeighbours: 3
-    }
-
-    expect(checkStatusForNextGeneration(cell)).toBe("alive")
-  })
-
-  it("should reproduce if surrounded by 3 living cells", () => {
-    const cell: Cell = {
-      status: "dead",
-      livingNeighbours: 3
-    }
-
-    expect(checkStatusForNextGeneration(cell)).toBe("alive")
-  })
-
-  it("should not reproduce if surrounded by 2 living cells", () => {
-    const cell: Cell = {
-      status: "dead",
-      livingNeighbours: 2
-    }
-
-    expect(checkStatusForNextGeneration(cell)).toBe("dead")
+  it("should be 1", () => {
+    expect(1).toBe(1)
   })
 });
